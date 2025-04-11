a2e80ba7fc0a039fc1e217d7515aa685044f33b6
2bys <tobias.weber.hh@gmail.com>
2025-04-11 20:07:55 +0200

    UPD: imports

--- laplax/curv/__init__.py
+++ laplax/curv/__init__.py
@@ -1,9 +1,17 @@
 """This module contains curvature-matrix-vector products and estimators."""
 
-from .cov import create_posterior_fn
+from .cov import (
+    create_posterior_fn,
+    estimate_curvature,
+    register_curvature_method,
+    set_posterior_fn,
+)
 from .ggn import create_ggn_mv
 
 __all__ = [
     "create_ggn_mv",
     "create_posterior_fn",
+    "estimate_curvature",
+    "register_curvature_method",
+    "set_posterior_fn",
 ]
