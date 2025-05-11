f8c13f179c80f5ac99b26cf981cb1290ec005480
lenardrommel <lenard.rommel@student.uni-tuebingen.de>
2025-05-11 17:25:22 +0200

    runs through but is very strange

--- examples/0004_fsp_integration.py
+++ examples/0004_fsp_integration.py
@@ -1,11 +1,16 @@
+from functools import partial
 import jax
 import jax.numpy as jnp
 import optax
 from flax import nnx
 from helper import DataLoader, get_sinusoid_example
 from matplotlib import pyplot as plt
-from plotting import plot_sinusoid_task, plot_gp_prediction
-
+from plotting import (
+    plot_sinusoid_task,
+    plot_gp_prediction,
+    plot_regression_with_uncertainty,
+)
+from laplax.curv.utils import LowRankTerms
 from laplax.curv.cov import Posterior
 from laplax.curv.fsp import create_fsp_objective
 from laplax.curv.lanczos_isqrt import lanczos_isqrt
@@ -19,6 +24,8 @@ from laplax.curv.fsp import (
     create_fsp_objective,
     lanczos_jacobian_initialization,
 )
+from laplax.curv.cov import set_posterior_fn
+from laplax.eval.pushforward import set_lin_pushforward
 from laplax.curv.lanczos_isqrt import lanczos_isqrt
 from helper import (
     DataLoader,
@@ -27,9 +34,35 @@ from helper import (
     RBFKernel,
     build_covariance_matrix,
 )
-
+from laplax.eval.pushforward import (
+    lin_pred_mean,
+    lin_pred_std,
+    lin_pred_var,
+    lin_setup,
+    set_lin_pushforward,
+)
 from laplax.enums import LossFn
-from laplax.types import Callable, Data, Float, ModelFn, Params, PredArray
+from laplax.types import (
+    Callable,
+    Data,
+    Float,
+    ModelFn,
+    Params,
+    PredArray,
+    PosteriorState,
+)
+from functools import partial
+
+from plotting import plot_regression_with_uncertainty
+
+from laplax.eval.pushforward import (
+    nonlin_pred_mean,
+    nonlin_pred_std,
+    nonlin_pred_var,
+    nonlin_setup,
+    set_nonlin_pushforward,
+)
+from laplax.curv.cov import Posterior
 
 jax.config.update("jax_enable_x64", True)
 
@@ -69,7 +102,21 @@ class Model(nnx.Module):
         return x
 
 
-model = Model(in_channels=1, hidden_channels=64, out_channels=1, rngs=nnx.Rngs(0))
+def to_float64(model):
+    graph_def, params = nnx.split(model)
+    params = laplax.util.tree.to_dtype(params, jnp.float64)
+    return nnx.merge(graph_def, params)
+
+
+model = to_float64(
+    Model(
+        in_channels=1,
+        hidden_channels=64,
+        out_channels=1,
+        rngs=nnx.Rngs(0),
+    )
+)
+
 graph_def, params = nnx.split(model)
 
 
@@ -108,6 +155,18 @@ def train_step(model, optimizer, x, y):
     return loss
 
 
+@nnx.jit
+def _train_step(model, optimizer, x, y):
+    def loss_fn(model):
+        y_pred = model(x)  # Call methods directly
+        return jnp.sum((y_pred - y) ** 2)
+
+    loss, grads = nnx.value_and_grad(loss_fn)(model)
+    optimizer.update(grads)  # Inplace updates
+
+    return loss
+
+
 def train_model(model, n_epochs, lr=1e-3):
     # Create optimizer
     optimizer = nnx.Optimizer(model, optax.adam(lr))  # Reference sharing
@@ -115,7 +174,7 @@ def train_model(model, n_epochs, lr=1e-3):
     # Train epoch
     for epoch in range(n_epochs):
         for x_tr, y_tr in train_loader:
-            loss = train_step(model, optimizer, x_tr, y_tr)
+            loss = _train_step(model, optimizer, x_tr, y_tr)
 
         if epoch % 100 == 0:
             print(f"[epoch {epoch}]: loss: {loss:.4f}")
@@ -124,8 +183,13 @@ def train_model(model, n_epochs, lr=1e-3):
     return model
 
 
-model = train_model(model, n_epochs=10)
+model = train_model(model, n_epochs=1000)
 data = {"inputs": X_train, "targets": y_train}
+X_pred = jnp.linspace(0.0, 8.0, 200).reshape(200, 1)
+y_pred = jax.vmap(model)(X_pred)
+
+_ = plot_sinusoid_task(X_train, y_train, X_test, y_test, X_pred, y_pred)
+plt.show()
 
 
 def create_fsp_ggn_mv(
@@ -169,8 +233,48 @@ def create_fsp_ggn_mv(
     return ggn_mv
 
 
-def compute_curvature():
-    pass
+def compute_curvature_fn(model_fn, params, ggn):
+    eigvals, eigvecs = jnp.linalg.eigh(ggn)
+    eigvals = jnp.flip(eigvals, axis=0)
+    eigvecs = jnp.flip(eigvecs, axis=1)
+
+    _, unravel_fn = ravel_pytree(params)
+
+    def compute_cov_sqrt(_u):
+        cov_sqrt = _u @ (eigvecs[:, ::-1] / jnp.sqrt(eigvals[::-1]))
+
+        def jvp(x, v):
+            return jax.jvp(lambda p: model_fn(x, p), (params,), (v,))[1]
+
+        def scan_fn(carry, i):
+            running_sum, truncation_idx = carry
+            lr_fac = unravel_fn(cov_sqrt[:, i])
+            sqrt_jvp = jax.vmap(lambda xc: jvp(xc, lr_fac) ** 2)(X_train)
+            pv = jnp.sum(sqrt_jvp)
+            new_running_sum = running_sum + pv
+            new_truncation_idx = jax.lax.cond(
+                (new_running_sum >= 150) & (truncation_idx == -1),
+                lambda _: i + 1,  # We found our index
+                lambda _: truncation_idx,  # Keep current value
+                operand=None,
+            )
+
+            return (new_running_sum, new_truncation_idx), sqrt_jvp
+
+        init_carry = (0.0, -1)
+        indices = jnp.arange(eigvals.shape[0])
+        (_, truncation_idx), post_var = jax.lax.scan(scan_fn, init_carry, indices)
+
+        truncation_idx = jax.lax.cond(
+            truncation_idx == -1,
+            lambda _: eigvals.shape[0],
+            lambda _: truncation_idx,
+            operand=None,
+        )
+
+        return cov_sqrt[:, :truncation_idx]
+
+    return compute_cov_sqrt
 
 
 def fsp_laplace(
@@ -210,41 +314,50 @@ def fsp_laplace(
     eigvals = jnp.flip(eigvals, axis=0)
     eigvecs = jnp.flip(eigvecs, axis=1)
 
-    cov_sqrt = _u @ (eigvecs[:, ::-1] / jnp.sqrt(eigvals[::-1]))
-    _, unravel_fn = jax.flatten_util.ravel_pytree(params)
-
+    # ==========
     params = laplax.util.tree.to_dtype(params, jnp.float64)
+    cov_sqrt = compute_curvature_fn(model_fn, params, ggn_matrix)(_u)
 
-    def jvp(x, v):
-        return jax.jvp(lambda p: model_fn(x, p), (params,), (v,))[1]
+    posterior_state: PosteriorState = {"scale_sqrt": cov_sqrt}
+    posterior = Posterior(
+        state=posterior_state,
+        cov_mv=lambda state: lambda x: state["scale_sqrt"] @ state["scale_sqrt"].T @ x,
+        scale_mv=lambda state: lambda x: state["scale_sqrt"] @ x,
+    )
+    curv_estimate = LowRankTerms(cov_sqrt, s, 1)
+    posterior_fn = set_posterior_fn("lanczos", curv_estimate, layout=params)
 
-    def scan_fn(carry, i):
-        running_sum, truncation_idx = carry
-        lr_fac = unravel_fn(cov_sqrt[:, i])
-        sqrt_jvp = jax.vmap(lambda xc: jvp(xc, lr_fac) ** 2)(X_train)
-        pv = jnp.sum(sqrt_jvp)
-        new_running_sum = running_sum + pv
-        new_truncation_idx = jax.lax.cond(
-            (new_running_sum >= 150) & (truncation_idx == -1),
-            lambda _: i + 1,  # We found our index
-            lambda _: truncation_idx,  # Keep current value
-            operand=None,
-        )
-
-        return (new_running_sum, new_truncation_idx), sqrt_jvp
-
-    init_carry = (0.0, -1)
-    indices = jnp.arange(eigvals.shape[0])
-    (_, truncation_idx), post_var = jax.lax.scan(scan_fn, init_carry, indices)
-
-    truncation_idx = jax.lax.cond(
-        truncation_idx == -1,
-        lambda _: eigvals.shape[0],
-        lambda _: truncation_idx,
-        operand=None,
+    set_nonlin_prob_predictive = partial(
+        set_nonlin_pushforward,
+        model_fn=model_fn,
+        mean_params=params,
+        posterior_fn=posterior_fn,
+        pushforward_fns=[
+            nonlin_setup,
+            nonlin_pred_mean,
+            nonlin_pred_var,
+            nonlin_pred_std,
+        ],
+        key=jax.random.key(42),
+        num_samples=10000,
+    )
+    prior_arguments = {"prior_prec": 1.0}  # Choose any prior precision.
+    prob_predictive = set_nonlin_prob_predictive(
+        prior_arguments=prior_arguments,
     )
 
-    cov_sqrt = cov_sqrt[:, :truncation_idx]
+    X_pred = jnp.linspace(0, 8, 200).reshape(200, 1)
+    pred = jax.vmap(prob_predictive)(X_pred)
+
+    _ = plot_regression_with_uncertainty(
+        X_train=data["inputs"],
+        y_train=data["targets"],
+        X_pred=X_pred,
+        y_pred=pred["pred_mean"][:, 0],
+        y_std=jnp.sqrt(pred["pred_var"][:, 0]),
+    )
+
+    plt.show()
 
 
 fsp_laplace(
