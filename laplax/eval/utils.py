f928022e0e610d559d2b6de15314f203abf82189
lenardrommel <lenard.rommel@student.uni-tuebingen.de>
2025-07-16 14:39:01 +0200

    latest version

--- laplax/eval/utils.py
+++ laplax/eval/utils.py
@@ -357,17 +357,17 @@ def evaluate_metrics_on_generator(
     if not all_results:
         return {}
 
-    def flatten_recursive(d: dict, sep: str = "_") -> dict[str, any]:
+    def flatten_dict(d: dict, sep: str = "_") -> dict[str, any]:
         items: dict[str, any] = {}
         for k, v in d.items():
             if isinstance(v, dict):
-                items.update(flatten_recursive(v, k, sep=sep))
+                items.update(flatten_dict(v, sep=sep))
             else:
                 items[k] = v
         return items
 
     # Flatten the results if they are nested dictionaries
-    all_results = [flatten_recursive(result) for result in all_results]
+    all_results = [flatten_dict(result) for result in all_results]
 
     # Get all metric names from the first result
     metric_names = all_results[0].keys()
