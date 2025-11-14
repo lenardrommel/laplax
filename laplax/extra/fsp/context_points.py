abd61b18437b75e03030a0d4f8ffc752c74cf23e
lenardrommel <lenard.rommel@student.uni-tuebingen.de>
2025-11-14 16:39:57 +0100

    Remove deprecated FSP module and related components for code cleanup

--- laplax/extra/fsp/context_points.py
+++
@@ -1,8 +0,0 @@
-"""Backward-compatibility shim for context_points.
-
-The implementation has moved to `laplax.util.context_points`.
-This module re-exports all public names to avoid breaking imports.
-"""
-
-from laplax.util.context_points import *  # noqa: F401,F403
-
